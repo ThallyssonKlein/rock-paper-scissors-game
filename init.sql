195e22b17edfdad62f7bff7e8806e52dd5abab74
thallyssonklein <thallyssonklein@gmail.com>
2024-09-28 18:37:46 -0300

    fix: init.sql

--- init.sql
+++ init.sql
@@ -1,15 +1,3 @@
-CREATE TABLE movement (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    value VARCHAR(8) NOT NULL,
-    salt VARCHAR(32) NOT NULL,
-    hash VARCHAR(64) NOT NULL,
-    player_id BIGINT NOT NULL,
-    game_id BIGINT NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (player_id) REFERENCES user (id),
-    FOREIGN KEY (game_id) REFERENCES game (id)
-);
-
 CREATE TABLE user (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
@@ -25,6 +13,19 @@ CREATE TABLE game (
     FOREIGN KEY (owner_id) REFERENCES user (id),
     FOREIGN KEY (winner_id) REFERENCES user (id)
 );
+
+CREATE TABLE movement (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    value VARCHAR(8) NOT NULL,
+    salt VARCHAR(32) NOT NULL,
+    hash VARCHAR(64) NOT NULL,
+    player_id BIGINT NOT NULL,
+    game_id BIGINT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (player_id) REFERENCES user (id),
+    FOREIGN KEY (game_id) REFERENCES game (id)
+);
+
 
 INSERT INTO user (name, password) VALUES ('server_player', 'secure_password');
 
