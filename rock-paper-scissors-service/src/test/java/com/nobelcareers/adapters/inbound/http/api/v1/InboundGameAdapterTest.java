6da54c9b4446ab9dacaba8c97f070c4857e31519
thallyssonklein <thallyssonklein@gmail.com>
2024-09-29 17:06:37 -0300

    fix: tests

--- rock-paper-scissors-service/src/test/java/com/nobelcareers/adapters/inbound/http/api/v1/InboundGameAdapterTest.java
+++ rock-paper-scissors-service/src/test/java/com/nobelcareers/adapters/inbound/http/api/v1/InboundGameAdapterTest.java
@@ -1,14 +1,14 @@
 package com.nobelcareers.adapters.inbound.http.api.v1;
 
-import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
-
+import com.nobelcareers.adapters.outbound.cache.OutboundRedisAdapter;
 import com.nobelcareers.adapters.outbound.database.OutboundGameAdapter;
 import com.nobelcareers.adapters.outbound.database.OutboundMovementAdapter;
 import com.nobelcareers.adapters.outbound.database.OutboundResultAdapter;
 import com.nobelcareers.domain.game.GameService;
+import com.nobelcareers.domain.game.bo.MovementBO;
 import com.nobelcareers.domain.game.bo.MovementValueBO;
 import com.nobelcareers.domain.game.bo.WinnerBO;
+import com.nobelcareers.domain.movement.MovementService;
 import com.nobelcareers.ports.inbound.http.api.v1.dto.OutboundGameResultDTO;
 import com.nobelcareers.ports.inbound.http.api.v1.exception.ForbiddenException;
 import com.nobelcareers.ports.inbound.http.api.v1.exception.NotFoundException;
@@ -23,10 +23,21 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Value;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
 class InboundGameAdapterTest {
 
     @Mock
     private GameService gameService;
+
+    @Mock
+    private MovementService movementService;
 
     @Mock
     private OutboundMovementAdapter outboundMovementAdapter;
@@ -40,94 +51,122 @@ class InboundGameAdapterTest {
     @Mock
     private OutboundResultAdapter outboundResultAdapter;
 
+    @Mock
+    private OutboundRedisAdapter outboundRedisAdapter;
+
     @InjectMocks
     private InboundGameAdapter inboundGameAdapter;
 
-    @Value("${game.server_player_id}")
-    private Long serverPlayerId;
+    private final Long serverPlayerId = 1L;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+        try {
+            Field serverPlayerIdField = InboundGameAdapter.class.getDeclaredField("serverPlayerId");
+            serverPlayerIdField.setAccessible(true);
+            serverPlayerIdField.set(inboundGameAdapter, serverPlayerId);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    void result_PlayerWins() throws ForbiddenException, NotFoundException {
+    void testNextServerMove_PlayerTurn_ThrowsForbiddenException() throws NotFoundException {
         Long gameId = 1L;
-        String playerMove = "ROCK";
         Long playerId = 2L;
 
         when(outboundGameAdapter.getStatusByGameId(gameId)).thenReturn(StatusDAO.OPENED);
-        MovementDAO lastServerMovement = new MovementDAO();
-        lastServerMovement.setValue(MovementValueDAO.PAPER);
-        when(outboundMovementAdapter.getLastServerMovementByGameId(gameId)).thenReturn(lastServerMovement);
-        when(gameService.getWinner(MovementValueBO.valueOf(playerMove), MovementValueBO.valueOf(lastServerMovement.getValue().name())))
-                .thenReturn(WinnerBO.PLAYER);
+        when(outboundRedisAdapter.get(gameId.toString())).thenReturn(playerId.toString());
 
-        OutboundGameResultDTO result = inboundGameAdapter.result(gameId, playerMove, playerId);
-
-        assertEquals(WinnerBO.PLAYER.name(), result.getResult());
-        verify(outboundResultAdapter, times(1)).saveResults(WinnerBO.PLAYER, gameId, playerId);
+        ForbiddenException exception = assertThrows(ForbiddenException.class, () -> inboundGameAdapter.nextServerMove(gameId, playerId));
+        assertEquals("It is your turn", exception.getMessage());
+        verify(metricCollector, times(1)).incrementMetric("try_to_get_next_server_move_from_his_turn");
     }
 
     @Test
-    void result_ServerWins() throws ForbiddenException, NotFoundException {
+    void testNextServerMove_ClosedGame_ThrowsForbiddenException() throws NotFoundException {
         Long gameId = 1L;
-        String playerMove = "ROCK";
-        Long playerId = 2L;
-
-        when(outboundGameAdapter.getStatusByGameId(gameId)).thenReturn(StatusDAO.OPENED);
-        MovementDAO lastServerMovement = new MovementDAO();
-        lastServerMovement.setValue(MovementValueDAO.SCISSORS);
-        when(outboundMovementAdapter.getLastServerMovementByGameId(gameId)).thenReturn(lastServerMovement);
-        when(gameService.getWinner(MovementValueBO.valueOf(playerMove), MovementValueBO.valueOf(lastServerMovement.getValue().name())))
-                .thenReturn(WinnerBO.SERVER);
-
-        OutboundGameResultDTO result = inboundGameAdapter.result(gameId, playerMove, playerId);
-
-        assertEquals(WinnerBO.SERVER.name(), result.getResult());
-        verify(outboundResultAdapter, times(1)).saveResults(WinnerBO.SERVER, gameId, playerId);
-    }
-
-    @Test
-    void result_Draw() throws ForbiddenException, NotFoundException {
-        Long gameId = 1L;
-        String playerMove = "ROCK";
-        Long playerId = 2L;
-
-        when(outboundGameAdapter.getStatusByGameId(gameId)).thenReturn(StatusDAO.OPENED);
-        MovementDAO lastServerMovement = new MovementDAO();
-        lastServerMovement.setValue(MovementValueDAO.ROCK);
-        when(outboundMovementAdapter.getLastServerMovementByGameId(gameId)).thenReturn(lastServerMovement);
-        when(gameService.getWinner(MovementValueBO.valueOf(playerMove), MovementValueBO.valueOf(lastServerMovement.getValue().name())))
-                .thenReturn(WinnerBO.DRAW);
-
-        OutboundGameResultDTO result = inboundGameAdapter.result(gameId, playerMove, playerId);
-
-        assertEquals(WinnerBO.DRAW.name(), result.getResult());
-        verify(outboundResultAdapter, times(1)).saveResults(WinnerBO.DRAW, gameId, playerId);
-    }
-
-    @Test
-    void result_GameClosed() throws NotFoundException {
-        Long gameId = 1L;
-        String playerMove = "ROCK";
         Long playerId = 2L;
 
         when(outboundGameAdapter.getStatusByGameId(gameId)).thenReturn(StatusDAO.CLOSED);
 
-        assertThrows(ForbiddenException.class, () -> inboundGameAdapter.result(gameId, playerMove, playerId));
+        ForbiddenException exception = assertThrows(ForbiddenException.class, () -> inboundGameAdapter.nextServerMove(gameId, playerId));
+        assertEquals("Game closed", exception.getMessage());
+        verify(metricCollector, times(1)).incrementMetric("try_to_get_next_server_move_from_closed_game");
     }
 
     @Test
-    void result_ServerMovementNotFound() throws NotFoundException {
+    void testNextServerMove_Successful() throws ForbiddenException, NotFoundException {
         Long gameId = 1L;
-        String playerMove = "ROCK";
         Long playerId = 2L;
+        List<MovementBO> movementsFromPlayer = Arrays.asList(new MovementBO());
 
         when(outboundGameAdapter.getStatusByGameId(gameId)).thenReturn(StatusDAO.OPENED);
-        when(outboundMovementAdapter.getLastServerMovementByGameId(gameId)).thenReturn(null);
+        when(outboundRedisAdapter.get(gameId.toString())).thenReturn(null); // Indica que é a vez do servidor
+        when(outboundMovementAdapter.findAllMovementsFromOnePlayer(playerId)).thenReturn(movementsFromPlayer);
+        when(movementService.generateServerMovement(movementsFromPlayer)).thenReturn("ROCK");
+        when(movementService.generateSalt()).thenReturn("randomSalt");
+        when(movementService.generateHash("ROCK", "randomSalt")).thenReturn("hashedValue");
 
-        assertThrows(NotFoundException.class, () -> inboundGameAdapter.result(gameId, playerMove, playerId));
+        String result = inboundGameAdapter.nextServerMove(gameId, playerId);
+
+        assertEquals("hashedValue", result);
+        verify(outboundMovementAdapter, times(1)).saveMovement(MovementValueDAO.valueOf("ROCK"), "randomSalt", "hashedValue", gameId, serverPlayerId);
+        verify(outboundRedisAdapter, times(1)).save(gameId.toString(), playerId.toString());
+    }
+
+    @Test
+    void testResult_NotHisTurn_ThrowsForbiddenException() throws NotFoundException {
+        Long gameId = 1L;
+        Long playerId = 2L;
+        String playerMove = "ROCK";
+
+        when(outboundGameAdapter.getStatusByGameId(gameId)).thenReturn(StatusDAO.OPENED);
+        when(outboundRedisAdapter.get(gameId.toString())).thenReturn("3"); // ID diferente do jogador atual
+
+        ForbiddenException exception = assertThrows(ForbiddenException.class, () -> inboundGameAdapter.result(gameId, playerMove, playerId));
+        assertEquals("Not your turn", exception.getMessage());
+        verify(metricCollector, times(1)).incrementMetric("try_to_get_result_from_not_his_turn");
+    }
+
+    @Test
+    void testResult_ClosedGame_ThrowsForbiddenException() throws NotFoundException {
+        Long gameId = 1L;
+        Long playerId = 2L;
+        String playerMove = "ROCK";
+
+        when(outboundGameAdapter.getStatusByGameId(gameId)).thenReturn(StatusDAO.CLOSED);
+
+        ForbiddenException exception = assertThrows(ForbiddenException.class, () -> inboundGameAdapter.result(gameId, playerMove, playerId));
+        assertEquals("Game closed", exception.getMessage());
+        verify(metricCollector, times(1)).incrementMetric("try_to_get_result_from_closed_game");
+    }
+
+    @Test
+    void testResult_Successful() throws ForbiddenException, NotFoundException {
+        Long gameId = 1L;
+        Long playerId = 2L;
+        String playerMove = "ROCK";
+
+        MovementDAO serverMovementDAO = new MovementDAO();
+        serverMovementDAO.setValue(MovementValueDAO.valueOf("PAPER"));
+        serverMovementDAO.setHash("hashValue");
+        serverMovementDAO.setSalt("randomSalt");
+
+        when(outboundGameAdapter.getStatusByGameId(gameId)).thenReturn(StatusDAO.OPENED);
+        when(outboundRedisAdapter.get(gameId.toString())).thenReturn(playerId.toString());
+        when(outboundMovementAdapter.getLastServerMovementByGameId(gameId)).thenReturn(serverMovementDAO);
+        when(gameService.getWinner(MovementValueBO.valueOf(playerMove), MovementValueBO.valueOf("PAPER"))).thenReturn(WinnerBO.SERVER);
+
+        OutboundGameResultDTO resultDTO = inboundGameAdapter.result(gameId, playerMove, playerId);
+
+        assertEquals("PAPER", resultDTO.getServerMove());
+        assertEquals("hashValue", resultDTO.getHash());
+        assertEquals("randomSalt", resultDTO.getSalt());
+        assertEquals("SERVER", resultDTO.getResult());
+
+        verify(outboundResultAdapter, times(1)).saveResults(WinnerBO.SERVER, gameId, playerId);
+        verify(outboundRedisAdapter, times(1)).save(gameId.toString(), serverPlayerId.toString());
     }
 }
